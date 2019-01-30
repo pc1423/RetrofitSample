c4b5c077fbb26662400f915248918f4f5063762b
ys <ys144756@gmail.com>
2019-01-30 18:55:13 +0900

    - retrofit 기본기능 구현

--- app/src/main/java/com/example/retrofitsample/MainActivity.java
+++ app/src/main/java/com/example/retrofitsample/MainActivity.java
@@ -2,12 +2,54 @@ package com.example.retrofitsample;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.retrofitsample.model.LottoResult;
+import com.example.retrofitsample.retrofit.LottoService;
+import com.jakewharton.retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory;
+
+import io.reactivex.Scheduler;
+import io.reactivex.android.schedulers.AndroidSchedulers;
+import io.reactivex.disposables.CompositeDisposable;
+import io.reactivex.functions.Consumer;
+import io.reactivex.schedulers.Schedulers;
+import retrofit2.Retrofit;
+import retrofit2.converter.gson.GsonConverterFactory;
 
 public class MainActivity extends AppCompatActivity {
+
+    private Retrofit retrofitService;
+    private CompositeDisposable disposable = new CompositeDisposable();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        retrofitService = new Retrofit.Builder()
+                .baseUrl("http://lotto.kaisyu.com/")
+                .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
+                .addConverterFactory(GsonConverterFactory.create())
+                .build();
+    }
+
+    public void onClickButton(View view) {
+        disposable.add(retrofitService
+                .create(LottoService.class)
+                .getLottoNumber(202)
+                .subscribeOn(Schedulers.io())
+                .observeOn(AndroidSchedulers.mainThread())
+                .subscribe(new Consumer<LottoResult>() {
+                    @Override
+                    public void accept(LottoResult lottoResult) {
+                        Toast.makeText(MainActivity.this, lottoResult.getNumberList().toString(), Toast.LENGTH_LONG).show();
+                    }
+                }, new Consumer<Throwable>() {
+                    @Override
+                    public void accept(Throwable throwable) {
+                        Toast.makeText(MainActivity.this, throwable.getMessage(), Toast.LENGTH_LONG).show();
+                    }
+                }));
     }
 }
